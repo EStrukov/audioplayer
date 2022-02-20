9cca2561e7e731ae32edb31d7c08a4998abd1c33
EStrukov <Jhezer1991@gmail.com>
2022-02-20 15:07:03 +0300

    vollume and progressbar

--- script.js
+++ script.js
@@ -3,9 +3,58 @@ const audio = new Audio(),
       next = document.querySelector('.next'),
       prev = document.querySelector('.prev'),
       title = document.querySelector('.name-song'),
+      rangeInput = document.querySelector('.progress-bar'),
+      rangeValue = document.querySelector('.volume-bar'),
+      valueButton = document.querySelector('.v-btn'),
       trackList = ['fan mode - asda',
                   'Fun_Mode_-_Steny_citadeli',
                   'Radio tapok - csacasd'];
+let numValue = 1;
+ //document.querySelector('.volume-bar').oninput = volume;
+/*
+  function volume() {
+  let v = this.value;
+  audio.volume = v / 100;
+}
+*/
+rangeValue.addEventListener('input', () => {
+    numValue = rangeValue.value / 100;
+    audio.volume = numValue;
+    document.querySelector('.volume-num').innerHTML = rangeValue.value;
+    const max = rangeValue.max;
+    const val = rangeValue.value;
+    
+    rangeValue.style.backgroundSize = val * 100 / max + '% 100%';
+    if(numValue === 0) {
+        valueButton.classList.add('vol-off');
+        valueButton.classList.remove('vol-on');
+    } else {
+        valueButton.classList.add('vol-on');
+        valueButton.classList.remove('vol-off');
+    }
+});
+valueButton.addEventListener('click', () => {
+    valueButton.classList.toggle('volume-icon-none');
+    valueButton.classList.toggle('volume-icon');
+    if(valueButton.classList.contains('volume-icon-none')) {
+        audio.volume = 0;
+        rangeValue.value = 0;
+        const max = rangeValue.max;
+        const val = rangeValue.value;
+        rangeValue.style.backgroundSize = val * 100 / max + '% 100%';
+        document.querySelector('.volume-num').innerHTML = 0;
+    } else {
+        if(numValue === 0) {
+            numValue += 0.01;
+        }
+        rangeValue.value = Math.trunc(numValue * 100);
+        const max = rangeValue.max;
+        const val = rangeValue.value;
+        rangeValue.style.backgroundSize = val * 100 / max + '% 100%';
+        audio.volume = numValue;
+        document.querySelector('.volume-num').innerHTML = Math.trunc(numValue * 100);
+    }
+});
 
 let isPlay = false,
     playNum = 0;
@@ -14,14 +63,14 @@ function playAudio() {
   audio.src = `assets/audio/${trackList[playNum]}.mp3`;
   //audio.currentTime = 0;
   if (!isPlay) {
-  audio.play();
-  isPlay = true;
+    audio.play();
+    isPlay = true;
     play.classList.remove('play');
-  play.classList.add('pause');
+    play.classList.add('pause');
   } else {
     audio.pause();
     isPlay = false;
-        play.classList.remove('pause');
+    play.classList.remove('pause');
     play.classList.add('play');
   }
 }
@@ -32,22 +81,58 @@ function playNext() {
  ++playNum;
  if (playNum >= trackList.length) {
     playNum = 0;
+    audio.src = `assets/audio/${trackList[playNum]}.mp3`;
     title.innerHTML = trackList[playNum];
-    playAudio();
+    audio.play();
+    isPlay = true;
+    play.classList.remove('play');
+    play.classList.add('pause');
+    }
+    audio.src = `assets/audio/${trackList[playNum]}.mp3`;
+    title.innerHTML = trackList[playNum];
+    audio.play();
+    isPlay = true;
+    play.classList.remove('play');
+    play.classList.add('pause');
   }
-  title.innerHTML = trackList[playNum];
-    playAudio();
-}
 next.addEventListener('click', playNext);
 
 function playPrev() {
-playNum--;
+  --playNum;
  if (playNum < 0) {
     playNum = trackList.length - 1;
     title.innerHTML = trackList[playNum];
-    playAudio();
+    audio.src = `assets/audio/${trackList[playNum]}.mp3`;
+    audio.play();
+    isPlay = true;
+    play.classList.remove('play');
+    play.classList.add('pause');
   }
-  playAudio();
-  title.innerHTML = trackList[playNum];
+    audio.src = `assets/audio/${trackList[playNum]}.mp3`;
+    audio.play();
+    isPlay = true;
+    play.classList.remove('play');
+    play.classList.add('pause');
+    title.innerHTML = trackList[playNum];
 }
 prev.addEventListener('click', playPrev);
+ //бегунок песни
+function redLine() {
+    const max = rangeInput.max
+    const val = rangeInput.value
+    
+    rangeInput.style.backgroundSize = val * 100 / max + '% 100%'
+    let time = Math.floor(val / 60) + ":"
+    if(val % 60 < 10) {
+        time += '0'
+    }
+    time += val % 60
+    document.querySelector('.this-time').innerHTML = time
+}
+
+rangeInput.addEventListener('input', () => {
+    const val = rangeInput.value
+    redLine()
+    audio.currentTime = val
+})
+
